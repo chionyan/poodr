5bad76ab7bddda609387d14b2da2f9a1c59f590b
chionyan <ariariarissa@gmail.com>
2018-12-13 17:02:34 +0900

    アクセサメソッドを使用する

--- chapter_2/gear.rb
+++ chapter_2/gear.rb
@@ -1,11 +1,13 @@
 class Gear
+  attr_reader :chainring, :cog
+
   def initialize(chainring, cog)
     @chainring = chainring
     @cog = cog
   end
 
   def ratio
-    @chainring / @cog.to_f
+    chainring / cog.to_f
   end
 end
 
