ceca0ab5dd022e6d1839f1c9db4af0bcca2d6e87
chionyan <ariariarissa@gmail.com>
2018-12-13 17:53:42 +0900

    Gear クラスに Wheel Struct を導入

--- chapter_2/gear.rb
+++ chapter_2/gear.rb
@@ -1,15 +1,27 @@
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @wheel = Wheel.new(rim, tire)
   end
 
   def ratio
     chainring / cog.to_f
   end
-end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+  def gear_inches
+    ratio * wheel_diameter
+  end
+
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2)
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
+  end
+end
