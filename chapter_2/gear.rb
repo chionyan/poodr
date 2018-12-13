304a14edf32076fdd4ba1b4a131894c5176cdd4c
chionyan <ariariarissa@gmail.com>
2018-12-13 17:00:41 +0900

    bad: アクセサメソッドを使わない

--- chapter_2/gear.rb
+++ chapter_2/gear.rb
@@ -1,25 +1,13 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
   end
 
   def ratio
-    chainring / cog.to_f
-  end
-
-  def gear_inches
-    # ギアインチ = 車輪の直径 * ギア比
-    # 車輪の直径 = リムの直径 + タイヤの厚みの2倍
-    ratio * (rim + (tire * 2))
+    @chainring / @cog.to_f
   end
 end
 
-# ArgumentError 発生
-# puts Gear.new(52, 11).ratio
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+puts Gear.new(52, 11).ratio
+puts Gear.new(30, 27).ratio
