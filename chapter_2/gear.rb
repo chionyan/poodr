cae9f91d40d31bb9ef8491ce1b51006fdba71c15
chionyan <ariariarissa@gmail.com>
2018-12-13 15:28:16 +0900

    簡単な Gear クラスの作成

--- chapter_2/gear.rb
+++ chapter_2/gear.rb
@@ -1,9 +1,15 @@
-chainring = 52
-cog = 11
-ratio = chainring / cog.to_f
-puts ratio
+class Gear
+  attr_reader :chainring, :cog
 
-chainring = 30
-cog = 27
-ratio = chainring / cog.to_f
-puts ratio
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio
+puts Gear.new(30, 27).ratio
