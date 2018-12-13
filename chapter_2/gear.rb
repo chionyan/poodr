47f5f46e6690e2114b471d4fdcb55672d0668618
chionyan <ariariarissa@gmail.com>
2018-12-13 17:58:51 +0900

    Wheel クラスの追加

--- chapter_2/gear.rb
+++ chapter_2/gear.rb
@@ -1,10 +1,10 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -12,16 +12,29 @@ class Gear
   end
 
   def gear_inches
-    ratio * wheel_diameter
-  end
-
-  def diameter(wheel)
-    wheel.rim + (wheel.tire * 2)
-  end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+    ratio * wheel.diameter
   end
 end
+
+# 本当はファイル分けるべき
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+wheel = Wheel.new(26, 1.5)
+puts wheel.circumference
+puts Gear.new(52, 11, wheel).gear_inches
+puts Gear.new(52, 11).ratio
