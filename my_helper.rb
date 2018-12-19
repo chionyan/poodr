8fdc3203958ee14f614e6c130390985c282e7c45
chionyan <ariariarissa@gmail.com>
2018-12-19 12:40:22 +0900

    同一ディレクトリのファイルを読み込めるメソッドを作成

---
+++ my_helper.rb
@@ -0,0 +1,10 @@
+# 自分以外の同じディレクトリのファイルを require する
+# main.rb でのみ使う
+#
+def my_require(file_name)
+  Dir.glob("./#{File.dirname(file_name)}/*") do |file|
+
+    next if file == "./#{file_name}"
+    require file
+  end
+end
