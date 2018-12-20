961386895b20dca7a4c6dad18ae8d6b72e4a46ed
chionyan <ariariarissa@gmail.com>
2018-12-20 14:25:04 +0900

    Gear の初期化の引数にハッシュを使う

--- chapter_3/gear.rb
+++ chapter_3/gear.rb
@@ -1,13 +1,12 @@
 require_relative 'wheel'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @rim = rim
-    @tire = tire
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
   end
 
   def gear_inches
@@ -16,10 +15,6 @@ class Gear
 
   def ratio
     chainring / cog.to_f
-  end
-
-  def wheel
-    @wheel ||= Wheel.new(rim, tire)
   end
 
   def diameter
--- chapter_3/main.rb
+++ chapter_3/main.rb
@@ -3,7 +3,11 @@ my_require __FILE__
 
 class Main
   def self.run
-    Gear.new(52, 11, 26, 1.5).gear_inches
+    Gear.new(
+      chainring: 52,
+      cog: 11,
+      wheel: Wheel.new(26, 1.5)
+    ).gear_inches
   end
 end
 
