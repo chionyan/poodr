ef28eb924d27924a0c8a23d32f170626411bf537
chionyan <ariariarissa@gmail.com>
2019-01-31 12:26:51 +0900

    具象クラスから始める

---
+++ chapter_6/bicycle.rb
@@ -0,0 +1,27 @@
+class Bicycle
+  attr_reader :size, :tape_color
+
+  def initialize(**args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  # 全ての自転車は、デフォルト値として同じタイヤサイズとチェーンサイズを持つ
+  def spares
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
+  end
+
+  # 他にも様々なメソッド
+end
+
+bike = Bicycle.new(
+  size: 'M',
+  tape_color: 'red'
+)
+
+p bike.size
+p bike.spares
