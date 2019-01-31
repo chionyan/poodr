7f565b4229322dbdb512fb4af7891eace24b67c1
chionyan <ariariarissa@gmail.com>
2019-01-31 14:34:40 +0900

    継承を不適切に適用する

--- chapter_6/bicycle.rb
+++ chapter_6/bicycle.rb
@@ -1,49 +1,19 @@
 class Bicycle
-  attr_reader :style, :size, :tape_color,
-              :front_shock, :rear_shock
+  attr_reader :size, :tape_color
 
-  def initialize(**args)
-    @style = args[:style]
+   def initialize(**args)
     @size = args[:size]
     @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
+   end
 
-  # 全ての自転車は、デフォルト値として同じタイヤサイズとチェーンサイズを持つ
+   # 全ての自転車は、デフォルト値として同じタイヤサイズとチェーンサイズを持つ
   def spares
-    # "style" の確認は、危険な道へ進む一歩
-    if style == :road
-      {
-        chain: '10-speed',
-        tire_size: '23', # milimeters
-        tape_color: tape_color
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1', # inches
-        rear_shock: rear_shock
-      }
-    end
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
   end
 
-  # 他にも様々なメソッド
+   # 他にも様々なメソッド
 end
-
-road_bike = Bicycle.new(
-  style: :road,
-  size: 'M',
-  tape_color: 'red'
-)
-
-p road_bike.spares
-
-mountain_bike = Bicycle.new(
-  style: :mountain,
-  size: 'S',
-  front_shock: 'Manitou',
-  rear_shock: 'Fox'
-)
-
-p mountain_bike.spares
---
+++ chapter_6/mountain_bike.rb
@@ -0,0 +1,24 @@
+require_relative 'bicycle'
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(**args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
+)
+
+p mountain_bike.size
+p mountain_bike.spares
