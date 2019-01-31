7871f5fbad216c7c9d3eb08b0379bee4e8beb5c8
chionyan <ariariarissa@gmail.com>
2019-01-31 12:53:12 +0900

    複数の型を埋め込む

--- chapter_6/bicycle.rb
+++ chapter_6/bicycle.rb
@@ -1,27 +1,49 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
 
   def initialize(**args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   # 全ての自転車は、デフォルト値として同じタイヤサイズとチェーンサイズを持つ
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    # "style" の確認は、危険な道へ進む一歩
+    if style == :road
+      {
+        chain: '10-speed',
+        tire_size: '23', # milimeters
+        tape_color: tape_color
+      }
+    else
+      {
+        chain: '10-speed',
+        tire_size: '2.1', # inches
+        rear_shock: rear_shock
+      }
+    end
   end
 
   # 他にも様々なメソッド
 end
 
-bike = Bicycle.new(
+road_bike = Bicycle.new(
+  style: :road,
   size: 'M',
   tape_color: 'red'
 )
 
-p bike.size
-p bike.spares
+p road_bike.spares
+
+mountain_bike = Bicycle.new(
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
+)
+
+p mountain_bike.spares
